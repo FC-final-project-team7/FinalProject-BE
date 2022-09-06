cc3dbaa9066f52e2dff7364b3a9bf2d08668a815
ho-jun97 <hojun9707@naver.com>
2022-09-07 00:27:58 +0900

    fix: 오류 고치기

--- src/main/java/com/aipark/domain/member/Member.java
+++ src/main/java/com/aipark/domain/member/Member.java
@@ -3,17 +3,10 @@ package com.aipark.domain.member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Getter
 @NoArgsConstructor
-@Entity
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
 }
