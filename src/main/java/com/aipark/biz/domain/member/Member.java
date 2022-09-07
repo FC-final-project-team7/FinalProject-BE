3694ecc29da6667af17d9a4e90f24696427ebc08
ho-jun97 <hojun9707@naver.com>
2022-09-07 17:43:23 +0900

    feat: Member Entity update

--- src/main/java/com/aipark/biz/domain/member/Member.java
+++ src/main/java/com/aipark/biz/domain/member/Member.java
@@ -3,10 +3,15 @@ package com.aipark.biz.domain.member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
 @NoArgsConstructor
+@Entity
 public class Member {
 
+    @Id
     private Long id;
 
 }
--- src/main/java/com/aipark/biz/domain/member/MemberRepository.java
+++ src/main/java/com/aipark/biz/domain/member/MemberRepository.java
@@ -1,4 +1,6 @@
 package com.aipark.biz.domain.member;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
