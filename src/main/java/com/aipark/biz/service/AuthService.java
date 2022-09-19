53c8989d53677a8d2bd8672f89089382dcfb250f
ho-jun97 <hojun9707@naver.com>
2022-09-19 17:39:14 +0900

    feat: 회원가입 & 로그인 구현

---
+++ src/main/java/com/aipark/biz/service/AuthService.java
@@ -0,0 +1,49 @@
+package com.aipark.biz.service;
+
+import com.aipark.biz.domain.member.Member;
+import com.aipark.biz.domain.member.MemberRepository;
+import com.aipark.config.jwt.TokenProvider;
+import com.aipark.web.dto.MemberDto;
+import com.aipark.web.dto.TokenDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+
+    @Transactional
+    public MemberDto.MemberResponse signup(MemberDto.MemberRequest memberRequestDto) {
+        if (memberRepository.existsByUsername(memberRequestDto.getUsername())) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다");
+        }
+
+        Member member = memberRequestDto.toEntity(passwordEncoder);
+        return MemberDto.MemberResponse.of(memberRepository.save(member));
+    }
+
+    @Transactional
+    public TokenDto login(MemberDto.MemberRequest memberRequestDto) {
+        // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
+        UsernamePasswordAuthenticationToken authenticationToken = memberRequestDto.toAuthentication();
+
+        // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
+        //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenDto tokenDto = tokenProvider.createTokenDto(authentication);
+
+        // 5. 토큰 발급
+        return tokenDto;
+    }
+}
---
+++ src/main/java/com/aipark/web/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.aipark.web.controller;
+
+import com.aipark.biz.service.AuthService;
+import com.aipark.web.dto.MemberDto;
+import com.aipark.web.dto.TokenDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberDto.MemberResponse> signup(@RequestBody MemberDto.MemberRequest memberRequestDto){
+        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberDto.MemberRequest memberRequestDto) {
+        return ResponseEntity.ok(authService.login(memberRequestDto));
+    }
+}
