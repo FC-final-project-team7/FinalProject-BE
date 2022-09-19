0e542a812382522f61b2918673e2eb85b682dacc
ho-jun97 <hojun9707@naver.com>
2022-09-19 16:39:54 +0900

    feat: Create SecurityConfig

--- src/main/java/com/aipark/config/SecurityConfig.java
+++ src/main/java/com/aipark/config/SecurityConfig.java
@@ -1,4 +1,62 @@
 package com.aipark.config;
 
+import com.aipark.config.jwt.JwtAccessDeniedHandler;
+import com.aipark.config.jwt.JwtAuthenticationEntryPoint;
+import com.aipark.config.jwt.JwtSecurityConfig;
+import com.aipark.config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+//                .httpBasic().disable()    //https만을 사용
+                .csrf().disable()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .addFilter(corsFilter)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**"
+                        , "/products"
+                        , "/v2/api-docs"
+                        , "/swagger-resources/**"
+                        , "/swagger-ui.html" //"/swagger-ui/index.html"
+                        , "/webjars/**"
+                        , "/swagger/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return http.build();
+    }
 }
