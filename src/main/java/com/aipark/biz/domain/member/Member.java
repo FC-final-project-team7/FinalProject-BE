506a86cb5068ec7446c2cf2d41eb0acd8db5e28d
ho-jun97 <hojun9707@naver.com>
2022-09-19 15:41:03 +0900

    feat: Create Member

--- src/main/java/com/aipark/biz/domain/member/Member.java
+++ src/main/java/com/aipark/biz/domain/member/Member.java
@@ -1,18 +1,41 @@
 package com.aipark.biz.domain.member;
 
+import com.aipark.biz.domain.BaseTimeEntity;
+import com.aipark.biz.domain.enums.Authority;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name ="MEMBER")
 @Entity
-public class Member {
+public class Member extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
 
 }
 
