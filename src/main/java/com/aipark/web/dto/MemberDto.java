1293eb222b63c5392a040529ebe340fefd53a1de
ho-jun97 <hojun9707@naver.com>
2022-09-19 17:37:09 +0900

    feat: Create MemberDto

--- src/main/java/com/aipark/web/dto/MemberDto.java
+++ src/main/java/com/aipark/web/dto/MemberDto.java
@@ -1,4 +1,53 @@
 package com.aipark.web.dto;
 
+import com.aipark.biz.domain.enums.Authority;
+import com.aipark.biz.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class MemberDto {
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class MemberRequest{
+        private String username;
+        private String email;
+        private String password;
+        private String name;
+        private String phoneNumber;
+
+        public Member toEntity(PasswordEncoder passwordEncoder){
+            return Member.builder()
+                    .username(username)
+                    .email(email)
+                    .password(passwordEncoder.encode(password))
+                    .name(name)
+                    .authority(Authority.ROLE_USER)
+                    .build();
+        }
+        public UsernamePasswordAuthenticationToken toAuthentication() {
+            return new UsernamePasswordAuthenticationToken(email, password);
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class MemberResponse{
+        private String name;
+        private String email;
+
+        public static MemberResponse of(Member member){
+            return MemberResponse.builder()
+                    .name(member.getName())
+                    .email(member.getEmail())
+                    .build();
+        }
+    }
 }
