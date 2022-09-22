deaf2a29ad21a752ce4bcd389ac4f9b5950f7e41
JeongSu <zz1996zz@naver.com>
2022-09-23 01:08:47 +0900

    fix: 처음 상태로 전환

--- src/main/java/com/aipark/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/aipark/config/auth/PrincipalDetailsService.java
@@ -3,11 +3,16 @@ package com.aipark.config.auth;
 import com.aipark.biz.domain.member.Member;
 import com.aipark.biz.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -18,10 +23,17 @@ public class PrincipalDetailsService implements UserDetailsService {
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member memberEntity = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username + " 을 DB에서 찾지 못함"));
-        if (memberEntity == null) {
-            throw new UsernameNotFoundException("사용자 정보를 찾을 수 없음");
-        }
-        return new PrincipalDetails(memberEntity);
+        return memberRepository.findByUsername(username)
+                .map(this::createUserDetails)
+                .orElseThrow(()-> new UsernameNotFoundException(username + " 을 DB에서 찾지 못함"));
+    }
+
+    private UserDetails createUserDetails(Member member){
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
+
+        return new User(
+                member.getUsername(),
+                member.getPassword(),
+                Collections.singleton(grantedAuthority));
     }
 }
