819ba27d4f2b751e4a15c34cf87136cab76c2ba4
JeongSu <zz1996zz@naver.com>
2022-09-22 20:40:35 +0900

    feat: httpBasic 사용 해제

--- src/main/java/com/aipark/config/SecurityConfig.java
+++ src/main/java/com/aipark/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//                .httpBasic().disable()    //https만을 사용
+                .httpBasic().disable()    //https만을 사용
                 .csrf().disable()
 
                 .sessionManagement()
