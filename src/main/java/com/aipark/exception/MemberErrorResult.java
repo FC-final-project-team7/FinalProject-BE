cffed28f46a4dbfc5b3e71e57776d47c5560a3b3
ho-jun97 <hojun9707@naver.com>
2022-09-23 00:31:03 +0900

    fix: Fix HttpStatus

--- src/main/java/com/aipark/exception/MemberErrorResult.java
+++ src/main/java/com/aipark/exception/MemberErrorResult.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum MemberErrorResult {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾지 못하였습니다."),
+    NOT_FOUND(HttpStatus.UNAUTHORIZED, "회원을 찾지 못하였습니다."),
     BAD_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
 
     private final HttpStatus httpStatus;
