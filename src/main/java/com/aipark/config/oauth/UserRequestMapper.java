ba2090226759ce15248c708d759a09e3a3d2ef7a
JeongSu <zz1996zz@naver.com>
2022-09-23 02:49:24 +0900

    feat: UserRequestMapper 클래스 생성

---
+++ src/main/java/com/aipark/config/oauth/UserRequestMapper.java
@@ -0,0 +1,20 @@
+package com.aipark.config.oauth;
+
+import com.aipark.web.dto.MemberDto;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class UserRequestMapper {
+    public MemberDto.SignRequest toDto(OAuth2User oAuth2User) {
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        return MemberDto.SignRequest.builder()
+                .username((String) attributes.get("username"))
+                .email((String) attributes.get("email"))
+                .password((String) attributes.get("password"))
+                .name((String) attributes.get("name"))
+                .build();
+    }
+}
