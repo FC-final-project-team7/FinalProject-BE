d93249858d1b231b8fa0fb2a6aff7fad0554c402
JeongSu <zz1996zz@naver.com>
2022-09-22 21:48:00 +0900

    feat: PrincipalDetails 클래스 생성
    UserDetails와 OAuth2User를 구현한 PrincipalDetails 클래스 생성

---
+++ src/main/java/com/aipark/config/auth/PrincipalDetails.java
@@ -0,0 +1,65 @@
+package com.aipark.config.auth;
+
+import com.aipark.biz.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(member.getAuthority().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
