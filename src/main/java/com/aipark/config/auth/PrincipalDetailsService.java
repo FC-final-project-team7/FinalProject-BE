dceff8d1af76f90ce079d536319ba0ba6cbebbec
JeongSu <zz1996zz@naver.com>
2022-09-22 21:48:23 +0900

    feat: PrincipalDetailsService 클래스 수정
    OAuth를 사용하기 위해 수정

--- src/main/java/com/aipark/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/aipark/config/auth/PrincipalDetailsService.java
@@ -3,16 +3,11 @@ package com.aipark.config.auth;
 import com.aipark.biz.domain.member.Member;
 import com.aipark.biz.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -23,17 +18,10 @@ public class PrincipalDetailsService implements UserDetailsService {
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return memberRepository.findByUsername(username)
-                .map(this::createUserDetails)
-                .orElseThrow(()-> new UsernameNotFoundException(username + " 을 DB에서 찾지 못함"));
-    }
-
-    private UserDetails createUserDetails(Member member){
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
-
-        return new User(
-                member.getUsername(),
-                member.getPassword(),
-                Collections.singleton(grantedAuthority));
+        Member memberEntity = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username + " 을 DB에서 찾지 못함"));
+        if (memberEntity == null) {
+            throw new UsernameNotFoundException("사용자 정보를 찾을 수 없음");
+        }
+        return new PrincipalDetails(memberEntity);
     }
 }
