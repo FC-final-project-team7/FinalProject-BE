e3dd411a27dd7da9a3140208f1fabf7e0c0de389
JeongSu <zz1996zz@naver.com>
2022-09-28 20:56:37 +0900

    feat: TempAudioRepository 생성

---
+++ src/main/java/com/aipark/biz/domain/tempAudio/TempAudioRepository.java
@@ -0,0 +1,6 @@
+package com.aipark.biz.domain.tempAudio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TempAudioRepository extends JpaRepository<TempAudio, Long> {
+}
