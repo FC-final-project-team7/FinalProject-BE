396e77d3c48708821e39b20cc9644fae0023b5b2
JeongSu <zz1996zz@naver.com>
2022-09-28 19:48:48 +0900

    feat: 수정

--- src/main/java/com/aipark/config/SecurityConfig.java
+++ src/main/java/com/aipark/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
                         , "/webjars/**"
                         , "/swagger/**"
                         , "/favicon.ico"
-                        ,"/members/check-id"
+                        , "/members/check-id"
                 ).permitAll()
                 .anyRequest().authenticated()
 
