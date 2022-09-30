af2d2ee277571e2ab79ca197a7844fd6889bfbf4
JeongSu <zz1996zz@naver.com>
2022-09-30 16:36:13 +0900

    feat: 주석 추가 및 파이썬 서버와 통신하는 메소드 생성

--- src/main/java/com/aipark/biz/service/PythonServerService.java
+++ src/main/java/com/aipark/biz/service/PythonServerService.java
@@ -30,15 +30,22 @@ public class PythonServerService {
     private final TempAudioRepository tempAudioRepository;
     private final ProjectRepository projectRepository;
 
+    /**
+     * 입력 페이지에서 수정 페이지로 넘어갈 때, 파이썬에 문장별 음성 파일 생성 요청할 때,
+     * @param request
+     * @return
+     */
     public ProjectDto.ModificationPageResponse createSentenceAudioFile(PythonServerDto.CreateAudioRequest request) {
 
         List<String> sentences = divideSentence(request.getText());
 
+        // Http 통신 body에 들어갈 json 객체 생성
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("username", request.getUsername());
         jsonObject.put("text", sentences);
         jsonObject.put("narration", request.getNarration());
 
+        // webClient를 사용하여 서버간 통신
         PythonServerDto.CreateAudioResponse response = buildWebClient().post()
                 .uri("/audios/sentence")
                 .body(Mono.just(jsonObject.toString()), JSONObject.class)
@@ -46,19 +53,51 @@ public class PythonServerService {
                 .bodyToMono(PythonServerDto.CreateAudioResponse.class)
                 .block();
 
+        // 파이썬 서버에서 제대로 생성이 안되면 fail이 들어온다.
         if (response.getStatus().equals("fail")) {
             throw new PythonServerException(PythonServerErrorResult.AUDIO_CREATE_ERROR);
         }
 
+        // 오디오들을 임시 음성 테이블에 저장한다.
         saveTempAudio(response, request.getProjectId());
 
         return createDto(request, response);
     }
 
-    public void createAudioFile(PythonServerDto.CreateAudioRequest requestDto) {
+    /**
+     * 수정 페이지에서 아바타 선택 페이지로 넘어갈 때, 파이썬에 음성 파일 생성 요청할 때
+     * 생성버튼을 눌러서 음성을 요청할 때(문장별 음성 생성은 예외)
+     * @param requestDto
+     * @return
+     */
+    public PythonServerDto.AudioResponse createAudioFile(PythonServerDto.CreateAudioRequest requestDto) {
 
+        // Http 통신 body에 들어갈 json 객체 생성
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", requestDto.getUsername());
+        jsonObject.put("text", requestDto.getText());
+        jsonObject.put("narration", "none");
+
+        // webClient를 사용하여 서버간 통신
+        PythonServerDto.AudioResponse responseDto = buildWebClient().post()
+                .uri("/audios")
+                .body(Mono.just(jsonObject.toString()), JSONObject.class)
+                .retrieve()
+                .bodyToMono(PythonServerDto.AudioResponse.class)
+                .block();
+
+        // 파이썬 서버에서 제대로 생성이 안되면 fail이 들어온다.
+        if (responseDto.getStatus().equals("fail")) {
+            throw new PythonServerException(PythonServerErrorResult.AUDIO_CREATE_ERROR);
+        }
+
+        return responseDto;
     }
 
+    /**
+     * WebClient 기본 설정 메소드
+     * @return WebClient
+     */
     public WebClient buildWebClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8000")
@@ -66,10 +105,20 @@ public class PythonServerService {
                 .build();
     }
 
+    /**
+     * 문장을 나눠주는 메소드
+     * @param text
+     * @return
+     */
     public List<String> divideSentence(String text) {
         return Arrays.stream(text.split("[.]")).map(s -> s.concat(".")).collect(Collectors.toList());
     }
 
+    /**
+     * 문장별 음성들을 임시로 저장하는 메소드
+     * @param response
+     * @param projectId
+     */
     public void saveTempAudio(PythonServerDto.CreateAudioResponse response, Long projectId) {
         Project project = projectRepository.findById(projectId).orElseThrow(
                 () -> new ProjectException(ProjectErrorResult.PROJECT_NOT_FOUND));
@@ -79,6 +128,12 @@ public class PythonServerService {
         }
     }
 
+    /**
+     * ModificationPageResponse Dto로 변경해주는 메소드
+     * @param request
+     * @param response
+     * @return
+     */
     public ProjectDto.ModificationPageResponse createDto(PythonServerDto.CreateAudioRequest request, PythonServerDto.CreateAudioResponse response) {
         ProjectDto.ModificationPageResponse mpr = ProjectDto.ModificationPageResponse.of(request);
 
