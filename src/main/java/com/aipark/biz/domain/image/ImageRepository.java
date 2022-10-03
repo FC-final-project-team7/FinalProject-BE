cab5153818e11f24a7638249bab2fb7c1a322dcd
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-04 01:23:23 +0900

    feat: Avatar value 전달하는 메서드

--- src/main/java/com/aipark/biz/domain/image/ImageRepository.java
+++ src/main/java/com/aipark/biz/domain/image/ImageRepository.java
@@ -9,4 +9,7 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
     // 아바타 리스트 전달하는 메서드
     @Query("SELECT i From Image i WHERE i.category like 'AVATAR%'")
     List<Image> findImageByCategory();
+
+    // 아바타 밸류 전달하는 메서드
+    List<Image> findImagesByImageNameStartingWith(String condition);
 }
