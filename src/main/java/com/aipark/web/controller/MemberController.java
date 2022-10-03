f303e9ee124868a1ee471561658776ac19ec0600
ho-jun97 <hojun9707@naver.com>
2022-10-03 22:13:29 +0900

    comment: 주석 추가

--- src/main/java/com/aipark/web/controller/MemberController.java
+++ src/main/java/com/aipark/web/controller/MemberController.java
@@ -15,30 +15,31 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
+    /**
+     * 현재 로그인한 회원정보를 알아내는 API
+     * @return 회원정보
+     */
     @GetMapping
     public ResponseEntity<MemberDto.MemberResponse> getMyMemberInfo(){
         return ResponseEntity.ok(memberService.getMyInfo());
     }
 
+    /**
+     * 회원관리에서 비밀번호 변경 요청
+     * @ReuqestBody changeRequestDto
+     * @return 비밀번호 성공 String
+     */
     @PutMapping
     public ResponseEntity<String> changePassword(@RequestBody MemberDto.ChangeRequest changeRequestDto){
         memberService.changePassword(changeRequestDto);
         return ResponseEntity.ok("비밀번호 변경 성공하셨습니다.");
     }
 
-    @PostMapping("/find-id")
-    public ResponseEntity<String> findId(@RequestBody MemberDto.FindIdRequest requestDto){
-        log.info("아이디 조회 : {}",requestDto.toString());
-
-        return ResponseEntity.ok("이메일로 아이디를 보냈습니당");
-    }
-
-    @PostMapping("/find-pwd")
-    public ResponseEntity<String> findPwd(@RequestBody MemberDto.FindPwdRequest requestDto){
-        memberService.findPwd(requestDto);
-        return ResponseEntity.ok("회원이 존재합니다");
-    }
-
+    /**
+     * 비밀번호 찾기를 통한 비밀번호 변경
+     * @param requestDto
+     * @return
+     */
     @PostMapping("/edit-pwd")
     public ResponseEntity<String> editPwd(@RequestBody MemberDto.EditPwdRequest requestDto){
         memberService.editPwd(requestDto);
@@ -46,13 +47,21 @@ public class MemberController {
         return ResponseEntity.ok("비밀번호 변경되었습니다.");
     }
 
-
+    /**
+     * 회원탈퇴
+     * @return
+     */
     @DeleteMapping
     public ResponseEntity<String> memberDrop(){
         memberService.memberDrop();
         return ResponseEntity.ok("회원 탈퇴 성공하셨습니다.");
     }
 
+    /**
+     * 회원중복 체크
+     * @param checkIdRequest
+     * @return
+     */
     @GetMapping("/check-id")
     public ResponseEntity<String> memberCheck(@RequestBody MemberDto.CheckIdRequest checkIdRequest){
         if(memberService.memberCheck(checkIdRequest)){
