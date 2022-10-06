b909fb497c2698d28524105f9449e4ddd2c41d54
ho-jun97 <hojun9707@naver.com>
2022-10-06 17:19:20 +0900

    test: Remove Test 어노테이션

--- src/test/java/com/aipark/AiProjectApplicationTests.java
+++ src/test/java/com/aipark/AiProjectApplicationTests.java
@@ -3,10 +3,10 @@ package com.aipark;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AiProjectApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
